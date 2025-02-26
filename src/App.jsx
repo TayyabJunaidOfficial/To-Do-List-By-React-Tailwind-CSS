f4d9856670f452c41a90bfaccd98e303940be348
Tayyab Junaid <tayyabjunaidofficial@gmail.com>
2025-02-26 19:31:12 +0500

    Edit and Delete buttons added in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,76 @@
-import {useState} from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [addTodo, setAddTodo] = useState("")
-  const [todos, setTodos] = useState([])
+  const [addTodo, setAddTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState(null);
 
-  const handleAdd=()=>{
-    setTodos([...todos, addTodo]);
-    console.log(todos);
-    
-    setAddTodo("");
-  }
+  const handleAdd = () => {
+    if (edit !== null) {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === edit ? { ...todo, text: addTodo } : todo
+        )
+      );
+      setEdit(null);
+    } else {
+      setTodos([...todos, addTodo]);
+      setAddTodo("");
+    }
+
+    const handleEdit = (todo) => {
+      setAddTodo(todo.text);
+      setEdit(todo.id);
+    };
+
+    const handleDelete = (id) =>
+      setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
     <>
       <Navbar />
       <div className="container m-auto bg-violet-200 min-w-screen p-4">
         <h1 className="text-xl font-bold">Add a Task</h1>
         <div className="py-1">
-          <input type="text" name="add" value={addTodo} onChange={(e)=>{setAddTodo(e.target.value)}} className="border-1 bg-amber-50 w-56" />
-          <button onClick={handleAdd} className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-4 py-0.5 rounded-lg mx-3 cursor-pointer">
-            Add
+          <input
+            type="text"
+            name="add"
+            value={addTodo}
+            onChange={(e) => {
+              setAddTodo(e.target.value);
+            }}
+            className="border-1 bg-amber-50 w-56"
+          />
+          <button
+            onClick={handleAdd}
+            className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-4 py-0.5 rounded-lg mx-3 cursor-pointer"
+          >
+            {edit ? "Update" : "Add"}
           </button>
         </div>
         <h1 className="text-xl font-bold">Your Tasks</h1>
-       {todos.map((todo)=>{
-        return  <div key={todo} className="flex justify-between items-center max-w-76 my-1 py-1 gap-2">
-         <div className="flex justify-center items-center">{todo}</div>
-         <div>
-           <button className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer">
-             Edit
-           </button>
-           <button className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer">
-             Delete
-           </button>
-         </div>
-       </div>
-       })}
+        {todos.map((todo) => {
+          return (
+            <div
+              key={todo}
+              className="flex justify-between items-center max-w-76 my-1 py-1 gap-2"
+            >
+              <div className="flex justify-center items-center">{todo}</div>
+              <div>
+                <button
+                  onClick={() => handleEdit(todo)}
+                  className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer"
+                >
+                  Edit
+                </button>
+                <button onClick={()=>handleDelete(todo)} className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer">
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );

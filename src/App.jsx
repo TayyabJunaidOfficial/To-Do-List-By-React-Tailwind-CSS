74de9091236f245063820471f84da8e243a50492
Tayyab Junaid <tayyabjunaidofficial@gmail.com>
2025-02-26 21:07:20 +0500

    Some Styling Added

--- src/App.jsx
+++ src/App.jsx
@@ -25,53 +25,63 @@ function App() {
     setEdit(todo.id);
   };
 
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
-  }
+  };
   return (
     <>
-      <Navbar />
-      <div className="container m-auto bg-violet-200 min-w-screen p-4">
-        <h1 className="text-xl font-bold">{edit ? "Edit Task" : "Add a Task"}</h1>
-        <div className="py-1">
-          <input
-            type="text"
-            name="add"
-            value={addTodo}
-            onChange={(e) => {
-              setAddTodo(e.target.value);
-            }}
-            className="border-1 bg-amber-50 w-56"
-          />
-          <button
-            onClick={handleAdd}
-            className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-4 py-0.5 rounded-lg mx-3 cursor-pointer"
-          >
-            {edit ? "Update" : "Add"}
-          </button>
-        </div>
-        <h1 className="text-xl font-bold">Your Tasks</h1>
-        {todos.map((todo) => {
-          return (
-            <div
-              key={todo.id}
-              className="flex justify-between items-center max-w-76 my-1 py-1 gap-2"
+      <div className="bg-violet-100 min-h-screen">
+        <Navbar />
+        <div className="border-2 bg-violet-200 shadow-xl w-fit rounded-xl m-auto my-2 p-4">
+          <h1 className="text-xl text-center font-bold">
+            iTask - Manage your to-do's at one place
+          </h1>
+          <h1 className="text-xl pt-2 font-bold">
+            {edit ? "Edit Task" : "Add a Task"}
+          </h1>
+          <div className="my-2">
+            <input
+              type="text"
+              name="add"
+              value={addTodo}
+              onChange={(e) => {
+                setAddTodo(e.target.value);
+              }}
+              className="border-1 bg-white w-md rounded-xl"
+            />
+            <button
+              onClick={handleAdd}
+              className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-4 py-0.5 rounded-xl mx-3 cursor-pointer"
             >
-              <div className="flex justify-center items-center">{todo.text}</div>
-              <div>
-                <button
-                  onClick={() => handleEdit(todo)}
-                  className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer"
-                >
-                  Edit
-                </button>
-                <button onClick={()=>handleDelete(todo.id)} className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-lg mx-1 cursor-pointer">
-                  Delete
-                </button>
+              {edit ? "Update" : "Add"}
+            </button>
+          </div>
+          <h1 className="text-xl pt-2 font-bold">Your Tasks</h1>
+          {todos.map((todo) => {
+            return (
+              <div
+                key={todo.id}
+                className=" py-1 flex justify-center my-2 gap-2"
+              >
+                <div className="w-96 rounded-xl ">{todo.text}</div>
+                <div>
+                  <button
+                    onClick={() => handleEdit(todo)}
+                    className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-xl mx-1 cursor-pointer"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(todo.id)}
+                    className="bg-violet-600 text-white font-bold hover:bg-violet-800 px-3 py-0.5 rounded-xl mx-1 cursor-pointer"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
